5ffb15e89041b4eea439b06ae3e46dc9f3c8a355
Shruti <shrutipulstya9@gmail.com>
2020-06-07 21:47:47 +0530

    added insertion sort

---
+++ insertionsort.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void insertionsort(int *A , int n)
+{
+    
+}
+
+int main()
+{
+    int A[] = {9,2,4,6,1};
+    insertionsort(A,5);
+    for(int i = 0; i < 5; i++)
+    {
+        cout << A[i]<<" ";
+    }
+    return 0;
+}
