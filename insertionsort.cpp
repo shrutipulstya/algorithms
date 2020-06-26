d7eac1fedb8e81c4c7dc1fed4325d2301f75b3a4
Shruti <shrutipulstya9@gmail.com>
2020-06-26 15:18:42 +0530

    added heapsort to the repository

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,18 +1,29 @@
 #include<iostream>
 using namespace std;
 
-void insertionsort(int *A , int n)
+void insertsort(int a[], int n)
 {
+    for(int i = 1; i < n; i++)
+    {   
+        int temp = a[i];
+        int j = i - 1;
+        while(j >= 0 && temp < a[j])
+        {
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = temp;
+    }
     
 }
 
 int main()
 {
-    int A[] = {9,2,4,6,1};
-    insertionsort(A,5);
-    for(int i = 0; i < 5; i++)
+    int a[] = {4,2,12,6,8,10};
+    insertsort(a,6);
+    for(int i = 0; i < 6; i++)
     {
-        cout << A[i]<<" ";
+        cout << a[i]<<" ";
     }
     return 0;
-}
+}   
